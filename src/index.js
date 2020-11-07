d9b549eacb72de592e8b5c2d91afc33ca945b495
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-11-06 22:52:21 -0600

    Added custom fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import './custom.scss';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
