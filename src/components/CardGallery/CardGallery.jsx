ce81d7ca9cda6cb1005928e24b241ebbc37c422e
margueritemb3 <bruggermarguerite@gmail.com>
2020-11-07 05:34:26 -0600

    added image overlay for cards

--- src/components/CardGallery/CardGallery.jsx
+++ src/components/CardGallery/CardGallery.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import PlayerCard from '../PlayerCard';
-import MockPlayers from './mockplayers.json';
+import React from "react";
+import PlayerCard from "../PlayerCard";
+import MockPlayers from "./mockplayers.json";
+import "./CardGallery.scss";
+import { Card } from "react-bootstrap";
 
 const CardGallery = () => {
-    return MockPlayers.map((player, i) => {
-        return (
-            <PlayerCard
-                key={i}
-                username={player.username}
-                bio={player.bio}
-                game={player.game}
-                rank={player.rank}
-                role={player.role}
-            />
-        );
-    });
+    return (
+        <div className="card-gallery">
+            {" "}
+            {MockPlayers.map((player, i) => {
+                return (
+                    <PlayerCard
+                        key={i}
+                        pic={player.pic}
+                        username={player.username}
+                        bio={player.bio}
+                        game={player.game}
+                        rank={player.rank}
+                        role={player.role}
+                    />
+                );
+            })}
+        </div>
+    );
 };
 
 export default CardGallery;
--- src/components/PlayerCard/PlayerCard.jsx
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
-import './PlayerCard.scss';
+import React from "react";
+import { Card, CardDeck } from "react-bootstrap";
+import "./PlayerCard.scss";
 
 const PlayerCard = (props) => {
     return (
@@ -9,35 +9,38 @@ const PlayerCard = (props) => {
                 bg="primary"
                 text="info"
                 style={{
-                    width: '18rem'
+                    width: "18rem"
                 }}
             >
-                <Card.Body>
-                    <Card.Title
-                        style={{
-                            fontFamily: "'Saira Stencil One', sans-serif"
-                        }}
-                    >{`${props.username}`}</Card.Title>
-                    <Card.Text>{`${props.bio}`}</Card.Text>
-                    <Card.Text className="card-attribute">
-                        <span>Game:</span>
-                        <span className="card-attribute-value">
-                            {props.game}
-                        </span>
-                    </Card.Text>
-                    <Card.Text className="card-attribute">
-                        <span>Rank:</span>
-                        <span className="card-attribute-value">
-                            {props.rank}
-                        </span>
-                    </Card.Text>
-                    <Card.Text className="card-attribute">
-                        <span>Role:</span>
-                        <span className="card-attribute-value">
-                            {props.role}
-                        </span>
-                    </Card.Text>
-                </Card.Body>
+                <Card.Img className="card-image" src={`${props.pic}`} />
+                <Card.ImgOverlay>
+                    <Card.Body>
+                        <Card.Title
+                            style={{
+                                fontFamily: "'Saira Stencil One', sans-serif"
+                            }}
+                        >{`${props.username}`}</Card.Title>
+                        <Card.Text>{`${props.bio}`}</Card.Text>
+                        <Card.Text className="card-attribute">
+                            <span>Game:</span>
+                            <span className="card-attribute-value">
+                                {props.game}
+                            </span>
+                        </Card.Text>
+                        <Card.Text className="card-attribute">
+                            <span>Rank:</span>
+                            <span className="card-attribute-value">
+                                {props.rank}
+                            </span>
+                        </Card.Text>
+                        <Card.Text className="card-attribute">
+                            <span>Role:</span>
+                            <span className="card-attribute-value">
+                                {props.role}
+                            </span>
+                        </Card.Text>
+                    </Card.Body>
+                </Card.ImgOverlay>
             </Card>
         </article>
     );
